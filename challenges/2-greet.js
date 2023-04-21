da6b2798603bb0fda63e2972b0c2cadd3fe2cd16
MWyssy <mike@wyss.co.uk>
2023-04-21 14:53:49 +0100

    add greet refactor to make it reusable

--- challenges/2-greet.js
+++ challenges/2-greet.js
@@ -6,11 +6,23 @@ const question = {
     message: 'What is your name?',
 };
 
+const goAgain = {
+    type: 'list',
+    name: 'goAgain',
+    message: 'Would you like to have another go?',
+    choices: ['Yes', 'No']
+}
+
 function askName() {
     inquirer
         .prompt(question)
-        .then((firstAnswers) => {
-            console.log(`Hello ${firstAnswers.name}!`)
+        .then((answer) => {
+            console.log(`Hello ${answer.name}!`)
+            inquirer
+            .prompt(goAgain)
+            .then((wouldLike) => {
+                if (wouldLike.goAgain == 'Yes') askName();
+            });
         });
 };
 
