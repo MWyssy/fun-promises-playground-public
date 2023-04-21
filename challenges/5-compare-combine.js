e2e007846bc55dbc9ddd273143013d4cd57d27ff
MWyssy <mike@wyss.co.uk>
2023-04-21 15:18:58 +0100

    add spinner funcationality to compare-combine

--- challenges/5-compare-combine.js
+++ challenges/5-compare-combine.js
@@ -1,4 +1,5 @@
 const fsPromise = require('fs/promises');
+const Spinner = require('cli-spinner').Spinner;
 
 const readSecetMessage = fsPromise.readFile('./secret-message.txt')
     .then((data) => {
@@ -18,13 +19,20 @@ const readSuperSecretMessage = fsPromise.readFile('./super-secret-message.txt')
 
 Promise.all([readSecetMessage, readSuperSecretMessage])
     .then((array) => {
+        const spinner = new Spinner('reading and merging files.. %s');
+        spinner.setSpinnerString('|/-\\');
+        spinner.start();
         if (array[0].length > array[1].length) {
             console.log(`secret-message.txt is larger, it has ${array[0].length - array[1].length} more characters in it.`);
         } else {
             console.log(`super-secret-message.txt is larger, it has ${array[1].length - array[0].length} more characters in it.`)
         };
         const combinedFiles = array[0] + array[1]
-        return fsPromise.writeFile('./mega-secret-message.txt', combinedFiles);
+        fsPromise.writeFile('./mega-secret-message.txt', combinedFiles);
+        setTimeout(() => {
+            console.log('merge complete!')
+            return spinner.stop(true);
+        }, 2000)
     })
     .catch((err) => {
         console.log(err)
